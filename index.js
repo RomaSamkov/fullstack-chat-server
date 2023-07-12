51f8b5dd396f36de55f13f67817edef233a4cc54
RomaSamkov <Romansamkov.rs@gmail.com>
2023-07-12 13:46:26 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+require("dotenv").config();
+
+const KEY = process.env.KEY;
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
